1195c40e7583814cb8e808e03e8c29a3fcac560f
Sednan22 <manocraftpt@gmail.com>
2026-05-11 19:50:48 +0200

    testing playwright + stealth in go

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Sednan22/tests/website"
-)
+import "fmt"
 
 func main() {
 
-	issues, err := website.ReadCacheIssues()
+	err := FetchWebSite()
 	if err != nil {
-		issues, err = website.FetchIssues("golang", "go")
-		if err != nil {
-			fmt.Printf("Error fetching issues: %v\n", err)
-			return
-		}
+		fmt.Printf("Error in fetchWebSite: %v", err)
+		return
 	}
 
-	for i, issue := range issues {
-		fmt.Printf("Issue: %d - %s - Author: %s\n", i, issue.Title, issue.User.Login)
-	}
-
-	fmt.Println("\nDONE...")
+	fmt.Println("Done")
 }
