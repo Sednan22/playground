8f81299bdb42f01b74298f9d377e551df87ac6f9
Sednan22 <manocraftpt@gmail.com>
2026-08-23 13:08:34 +0200

    ...

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/Sednan22/tests
 
 go 1.25.0
-
-require (
-	github.com/golang-jwt/jwt/v5 v5.3.1
-	github.com/joho/godotenv v1.5.1
-)
--- main.go
+++ main.go
@@ -2,38 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error loading .env")
-	}
-
-	cfgTokens, err := NewConfig()
-	if err != nil {
-		fmt.Println("error getting new config for jwt tokens")
-		return
-	}
-
-	// using a new create token
-	token, err := CreateToken(cfgTokens, "1")
-	if err != nil {
-		fmt.Println("error creating new access token")
-		return
-	}
-
-	// customToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
-	_, err = ValidateToken(token, cfgTokens.TokenSecret)
-	if err != nil {
-		fmt.Println("error validating token", err)
-		return
-	}
-
-	fmt.Println("valid token")
+	fmt.Println("playground")
 
 }
