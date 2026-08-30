2f2eb2b137140b790c0ccbf3b54bff01a3eb2661
Sednan22 <manocraftpt@gmail.com>
2026-08-30 13:03:43 +0200

    ...

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Create db schema and query in internal")
+	start := time.Now()
+	loop := 0
+	randNumber := int64(10000)
+	randomness := int64(0)
+	for {
+		randNumber += rand.Int63n(10)
+		randomness += rand.Int63n(10)
 
+		if randomness > randNumber {
+			fmt.Printf("RandNumber number is: %d\n", randNumber)
+			fmt.Printf("DoNothing number is: %d\n", randomness)
+			fmt.Printf("It took %v and %d loops\n", time.Since(start), loop)
+			break
+		}
+		loop++
+	}
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/Sednan22/tests
 
 go 1.25.0
-
-require golang.org/x/net v0.58.0
