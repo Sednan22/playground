8ae70bdd82b7ad836902b1d4dc55e96c8cbb13c1
Sednan22 <146624939+Sednan22@users.noreply.github.com>
2026-08-31 12:55:44 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -7,21 +7,5 @@ import (
 )
 
 func main() {
-
-	start := time.Now()
-	loop := 0
-	randNumber := int64(10000)
-	randomness := int64(0)
-	for {
-		randNumber += rand.Int63n(10)
-		randomness += rand.Int63n(10)
-
-		if randomness > randNumber {
-			fmt.Printf("RandNumber number is: %d\n", randNumber)
-			fmt.Printf("DoNothing number is: %d\n", randomness)
-			fmt.Printf("It took %v and %d loops\n", time.Since(start), loop)
-			break
-		}
-		loop++
-	}
+	fmt.Println("Hello World!")
 }
