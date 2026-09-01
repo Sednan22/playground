b4e5965a6ef40d5cd5b28aa7a44e7764d7cef77b
Sednan22 <146624939+Sednan22@users.noreply.github.com>
2026-09-01 14:32:43 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,34 +9,52 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Account struct {
+	Name string
+	API string
+}
+
 func main() {
 
 	accountID := flag.Int("account", 0, "account id")
 	flag.Parse()
-
-	accountLoaded := loadConfig(*accountID)
+	
 	if *accountID == 0 {
 		log.Fatal("--account=x needed")
 	}
 
+	accountLoaded, err := loadConfig(*accountID)
+	if err != nil {
+		log.Fatal("accountLoaded failed")
+	}
+	
 	fmt.Println(accountLoaded)
 }
 
-func loadConfig(accountID int) string {
+func loadConfig(accountID int) (Account, error) {
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("error: %v", err)
 	}
 
 	if accountID == 1 {
-		return os.Getenv("TEST1")
+		return Account{
+			Name: "test1",
+			API: os.Getenv("TEST1")
+		},nil
 	}
 	if accountID == 2 {
-		return os.Getenv("TEST2")
+		return Account{
+			Name: "test2",
+			API: os.Getenv("TEST2")
+		},nil
 	}
 	if accountID == 3 {
-		return os.Getenv("TEST3")
+		return Account{
+			Name: "test3",
+			API: os.Getenv("TEST3")
+		},nil
 	}
 
-	return "0"
+	return Account{}, fmt.Error("account not found")
 }
